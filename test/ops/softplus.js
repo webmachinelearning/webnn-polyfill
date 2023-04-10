9c35e7c44fcceda334c3c151e37cdcd67b9f0746
BruceDai <feng.dai@intel.com>
2023-04-10 04:06:55 +0000

    Deploying to gh-pages from @ webmachinelearning/webnn-polyfill@04a481f1fc2ee1edb4ae2dd86ec5c1de4a73635e 🚀

---
+++ test/ops/softplus.js
@@ -0,0 +1,43 @@
+'use strict';
+import * as utils from '../utils.js';
+
+describe('test softplus', () => {
+  let context;
+  before(async () => {
+    context = await navigator.ml.createContext();
+  });
+
+  async function testSoftplus(input, expected, options = {}) {
+    const builder = new MLGraphBuilder(context);
+    const x = builder.input('x', {type: 'float32', dimensions: input.shape});
+    const y = builder.softplus(x, options);
+    const graph = await builder.build({y});
+    const inputs = {'x': new Float32Array(input.value)};
+    const outputs = {'y': new Float32Array(utils.sizeOfShape(input.shape))};
+    const result = await context.compute(graph, inputs, outputs);
+    utils.checkValue(result.outputs.y, expected);
+  }
+
+  it('softplus default', async () => {
+    await testSoftplus(
+        {
+          shape: [4],
+          value: [-1, 0, 1, 2],
+        },
+        [0.313261688, 0.693147181, 1.313261688, 2.126928011],
+    );
+  });
+
+  it('softplus', async () => {
+    await testSoftplus(
+        {
+          shape: [4],
+          value: [-1, 0, 1, 2],
+        },
+        [0.063464023, 0.346573591, 1.063464045, 2.009074926],
+        {
+          steepness: 2,
+        },
+    );
+  });
+});
